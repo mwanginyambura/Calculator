89b2b10c17bae06e5ca2dca3131d809fb3bfd3c5
mwanginyambura <mwanginyambura@gmail.com>
2019-02-13 02:48:27 -0500

    Add js file

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+var add = function(number1, number2) {
+   return number1 + number2;
+   };
+   add();
+var subtract = function(number1, number2) {
+   return number1 - number2;
+   };
+   subtract();
+var multiply = function(number1, number2) {
+   return number1 * number2;
+   };
+   multipy();
+   var divide = function(number1, number2) {
+   return number1 / number2;
+   };
+   divide();
+var number1 = parseInt(prompt("enter a number:"));
+   alert(number1);
+   var number2 =parseInt(prompt("enter another number :"));
+   alert(number2);
+   var result = add(number1, number2);
+   alert (result);
