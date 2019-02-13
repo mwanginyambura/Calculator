7a0910587d20177ea1c1f3c5db54f753d5a753cd
mwanginyambura <mwanginyambura@gmail.com>
2019-02-13 04:16:18 -0500

    a remove the current js folder and put a new folder

--- calculator/script.js
+++ calculator/script.js
@@ -1,22 +1,35 @@
 var add = function(number1, number2) {
    return number1 + number2;
    };
-   add();
+
+var number1 = parseInt(prompt("enter a number:"));
+   alert(number1);
+   var number2 = parseInt(prompt("enter another number :"));
+   var result = add(number1, number2);
+   alert (result);
 var subtract = function(number1, number2) {
    return number1 - number2;
    };
    subtract();
+var number1 = parseInt(prompt("enter a number:"));
+   var number2 = parseInt(prompt("enter another number :"));
+   var result = subtract(number1, number2);
+   alert (result);
 var multiply = function(number1, number2) {
    return number1 * number2;
    };
    multipy();
+var number1 = parseInt(prompt("enter a number:"));
+   var number2 = parseInt(prompt("enter another number :"));
+   alert(number2);
+   var result = multiply(number1, number2);
+   alert (result);
    var divide = function(number1, number2) {
    return number1 / number2;
    };
    divide();
 var number1 = parseInt(prompt("enter a number:"));
    alert(number1);
-   var number2 =parseInt(prompt("enter another number :"));
-   alert(number2);
-   var result = add(number1, number2);
+   var number2 = parseInt(prompt("enter another number :"));
+   var result = divide(number1, number2);
    alert (result);
