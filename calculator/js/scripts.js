20eb88db9cae91f325820a4c6f41459afff9d953
mwanginyambura <mwanginyambura@gmail.com>
2019-02-13 02:12:08 -0500

    Add ne w files

---
+++ calculator/js/scripts.js
@@ -0,0 +1,40 @@
+var add = function(number1, number2) {
+   return number1 + number2;
+   };
+   add();
+var number1 = parseInt(prompt("enter a number:"));
+   alert(number1);
+   var number2 =parseInt(prompt("enter another number :"));
+   alert(number2);
+   var result = add(number1, number2);
+   alert (result);
+var subtract = function(number1, number2) {
+   return number1 - number2;
+   };
+   subtract();
+var number1 = parseInt(prompt("enter a number:"));
+   alert(number1);
+   var number2 =parseInt(prompt("enter another number :"));
+   alert(number2);
+   var result = subtract(number1, number2);
+   alert (result);
+var multiply = function(number1, number2) {
+   return number1 * number2;
+   };
+   multipy();
+var number1 = parseInt(prompt("enter a number:"));
+   alert(number1);
+   var number2 =parseInt(prompt("enter another number :"));
+   alert(number2);
+   var result = multiply(number1, number2);
+   alert (result);
+   var divide = function(number1, number2) {
+   return number1 / number2;
+   };
+   divide();
+var number1 = parseInt(prompt("enter a number:"));
+   alert(number1);
+   var number2 =parseInt(prompt("enter another number :"));
+   alert(number2);
+   var result = divide(number1, number2);
+   alert (result);
